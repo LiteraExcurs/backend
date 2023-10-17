35c3a87bcc48634124c406c636d1fb63283a2791
Gyxer513 <fpanchuk@yandex.ru>
2023-10-17 15:18:31 +0300

    swagger added

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  const config = new DocumentBuilder()
+  .setTitle('Litera excurs')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('litera')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   app.use(helmet());
   await app.listen(3000);
 }
