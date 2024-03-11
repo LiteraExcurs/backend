e82a5db4c8b5481ead10aec9f58aed5e75549bb4
Gyxer513 <102280477+Gyxer513@users.noreply.github.com>
2024-03-11 23:42:31 +0300

    bug fix

--- src/event/event.module.ts
+++ src/event/event.module.ts
@@ -4,7 +4,7 @@ import { EventController } from './event.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Event } from './entities/event.entity';
 import { ActivityModule } from '../activity/activity.module';
-import { GuidesModule } from "../guides/guides.module";
+import { GuidesModule } from '../guides/guides.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Event]), ActivityModule, GuidesModule],
